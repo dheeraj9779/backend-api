d98d21fc4fdd291f5d2471afbf259a418d3c608e
dheeraj9779 <dheerajsharma9170@gmail.com>
2025-05-09 18:30:14 +0530

    cloudinary addded

--- src/routes/img-manip.route.js
+++ src/routes/img-manip.route.js
@@ -7,6 +7,48 @@ const fs = require('fs').promises;
 const { v4: uuidv4 } = require('uuid');
 const cloudinary = require('cloudinary').v2
 
+
+// (async function() {
+
+//     // Configuration
+//     cloudinary.config({ 
+//         cloud_name: 'dxu6h32zq', 
+//         api_key: '888753991876936', 
+//         api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+//     });
+    
+//     // Upload an image
+//      const uploadResult = await cloudinary.uploader
+//        .upload(
+//            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//                public_id: 'shoes',
+//            }
+//        )
+//        .catch((error) => {
+//            console.log(error);
+//        });
+    
+//     console.log(uploadResult);
+    
+//     // Optimize delivery by resizing and applying auto-format and auto-quality
+//     const optimizeUrl = cloudinary.url('shoes', {
+//         fetch_format: 'auto',
+//         quality: 'auto'
+//     });
+    
+//     console.log(optimizeUrl);
+    
+//     // Transform the image: auto-crop to square aspect_ratio
+//     const autoCropUrl = cloudinary.url('shoes', {
+//         crop: 'auto',
+//         gravity: 'auto',
+//         width: 500,
+//         height: 500,
+//     });
+    
+//     console.log(autoCropUrl);    
+// })();
+
 //Using disk storage for image upload
 const imageStorage = multer.diskStorage({
     // Destination to store image     
@@ -43,12 +85,23 @@ const imageUpload = multer({
 
 //for multiple file upload
 /*imageUpload.array('myFile',2) */ 
+
+cloudinary.config({
+  cloud_name: 'dxu6h32zq',
+  api_key:'888753991876936',
+  api_secret: 'u9iPvYQu9lrt0lV4OYstHXmV0Qg'
+})
+
 router.post("/", imageUpload.single('myFile'),async (req, res) => {
     try{
-      cloudinary.v2.uploader.upload(req.file.path, {upload_preset: "my_preset"}, (error, result)=>{
+
+      cloudinary.uploader.upload(req.file.path, (error, result)=>{
         console.log(result, error);
+        res.send('Uploaded success')
       });
-      //console.log("File upload successfully")
+
+      
+      console.log("File upload successfully")
       const filePath = req.file.path;
       // Read the file from disk
       const fileBuffer = await fs.readFile(filePath);
